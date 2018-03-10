7e7a243d90a724c34528c730acebea27f8804d8c
Araba Taylor <araba.taylor20@gmail.com>
2018-03-10 03:08:59 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,4 +5,13 @@ class App < Sinatra::Base
   get '/name' do
     "My name is Araba."
   end
+
+  get '/hometown' do 
+    "My hometown is Orlando, FL."
+  end
+
+  get '/favorite-song' do 
+    "My favorite song is Sitting by the dock of the Bay by Otis Redding"
+  end
+  
 end
